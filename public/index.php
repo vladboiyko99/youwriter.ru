f8d3f11e9b6c93bff5df12248c8b117694fc4092
vladboiyko99 <vladboiyko99@mail.ru>
2018-12-07 13:21:54 +0300

    watch 6 lessons

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 require_once dirname(__DIR__) . '/config/init.php';
 require_once LIBS . '/function.php';
+require_once CONF . '/rouetes.php';
 
 new \youwriter\App();
 
-throw new Exception('Страница не найдена', 404);
--- vendor/youwriter/core/App.php
+++ vendor/youwriter/core/App.php
@@ -14,6 +14,7 @@ class App
         self::$app = Registry::instance();
         $this->getparsms();
         new ErrorHandier();
+        Router::dispatch($query);
 
     }
 
